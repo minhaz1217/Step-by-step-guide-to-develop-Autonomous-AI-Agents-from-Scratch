3f28d93dff81c0ebaf8f2147896d3bf5e8fd9397
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-05-27 23:36:18 +0600

    created generalized azure open ai as openAi

--- azure_open_ai.py
+++ azure_open_ai.py
@@ -4,20 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-client = AzureOpenAI(
+openAi = AzureOpenAI(
   azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), 
   api_key=os.getenv("AZURE_OPENAI_API_KEY"),
   api_version="2024-02-15-preview",
 )
 
-response = client.chat.completions.create(
-    model=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"), # model = "deployment_name".
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
-        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
-        {"role": "user", "content": "Do other Azure services support this too?"}
-    ]
-)
-
-print(response.choices[0].message.content)
+def test_azure_open_ai(): 
+  response = openAi.chat.completions.create(
+      model=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"), # model = "deployment_name".
+      messages=[
+          {"role": "system", "content": "You are a helpful assistant."},
+          {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+          {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+          {"role": "user", "content": "Do other Azure services support this too?"}
+      ]
+  )
+  print(response.choices[0].message.content)
