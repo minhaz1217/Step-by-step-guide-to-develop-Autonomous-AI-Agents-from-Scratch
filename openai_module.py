b06e519d91478dd56177a6622f798ef7df322678
Minhazul Hayat Khan <minhaz1217@gmail.com>
2025-05-30 01:26:15 +0600

    added conversation method for open ai module

--- openai_module.py
+++ openai_module.py
@@ -2,9 +2,10 @@ import os
 from azure_open_ai import openAi
 
 
+model = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
 def generate_text_basic(
     prompt: str,
-    model=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+    model=model,
     system_prompt: str = "You are a helpful bot that answers with max 3 lines.",
 ):
     response = openAi.chat.completions.create(
@@ -13,5 +14,12 @@ def generate_text_basic(
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": prompt},
         ],
+    )
+    return response.choices[0].message.content
+
+def generate_text_with_conversation(messages, model = model):
+    response = openAi.chat.completions.create(
+        model=model,
+        messages=messages
     )
     return response.choices[0].message.content
